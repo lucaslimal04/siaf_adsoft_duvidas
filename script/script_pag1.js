be2be7bd89c0f3896370e42271c05701910f1c5a
Lucas Lima Rodrigues <138540444+lucaslimal04@users.noreply.github.com>
2023-11-16 14:29:24 -0300

    atualizando

--- script/script_pag1.js
+++ script/script_pag1.js
@@ -1,15 +1,19 @@
 var schemas = document.querySelector('#schemas');
 var schemastxt = document.querySelector('#schemastxt');
 var clicker = true;
+var schemasimg = document.querySelector('#schemasimg');
+
 
 schemas.addEventListener('click', abrirSchemas);
 
 function abrirSchemas() {
     if (clicker) {
-        schemastxt.innerHTML += "<p>O SIAFW está mapeado incorretamente, clique com o botão direito do mouse no ícone do SIAFW e selecione abrir local do arquivo, o diretório dos arquivos que o SIAFW está, é direcionado pela rede, e precisa ser direcionado pelo servidor, faça o mapeamento do SIAFW de acordo a imagem. </p> <br> <img src="" alt="">";
+        schemastxt.innerHTML += "<p>O SIAFW está mapeado incorretamente, clique com o botão direito do mouse no ícone do SIAFW e selecione abrir local do arquivo, o diretório dos arquivos que o SIAFW está, é direcionado pela rede, e precisa ser direcionado pelo servidor, faça o mapeamento do SIAFW de acordo a imagem. </p>";
+        schemasimg.style.display = 'block';
     }
     else {
         schemastxt.innerHTML = "";
+        schemasimg.style.display = 'none'
     }
 
     clicker = !clicker;

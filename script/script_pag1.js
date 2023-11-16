7bdcc24ca90d767ebe75db501f82d16b28066bc0
Lucas Lima Rodrigues <138540444+lucaslimal04@users.noreply.github.com>
2023-11-16 15:45:09 -0300

    erro de schemas

--- script/script_pag1.js
+++ script/script_pag1.js
@@ -1,19 +1,24 @@
 var schemas = document.querySelector('#schemas');
 var schemastxt = document.querySelector('#schemastxt');
 var clicker = true;
-var schemasimg = document.querySelector('#schemasimg');
-
+var localschemas = document.querySelector('#localschemas');
+var schemastxtpos = document.querySelector('#schemastxtpos');
+var solucaoschemas = document.querySelector('#solucaoschemas');
 
 schemas.addEventListener('click', abrirSchemas);
 
 function abrirSchemas() {
     if (clicker) {
-        schemastxt.innerHTML += "<p>O SIAFW está mapeado incorretamente, clique com o botão direito do mouse no ícone do SIAFW e selecione abrir local do arquivo, o diretório dos arquivos que o SIAFW está, é direcionado pela rede, e precisa ser direcionado pelo servidor, faça o mapeamento do SIAFW de acordo a imagem. </p>";
-        schemasimg.style.display = 'block';
+        schemastxt.innerHTML += "<p>O SIAFW está mapeado incorretamente, clique com o botão direito do mouse no ícone do SIAFW e selecione abrir local do arquivo, o diretório dos arquivos que o SIAFW está, é direcionado pela rede conforme mostram as imagens.</p>";
+        localschemas.style.display = 'block';
+        schemastxtpos.innerHTML += "<p>O Sistema precisa ser direcionado pelo servidor, faça o mapeamento do SIAFW de acordo com a imagem e abra o SIAFW por esse mapeamento que conseguirá emitir notas normalmente.</p>";
+        solucaoschemas.style.display = 'block';
     }
     else {
         schemastxt.innerHTML = "";
-        schemasimg.style.display = 'none'
+        localschemas.style.display = 'none';
+        schemastxtpos.innerHTML = "";
+        solucaoschemas.style.display = 'none';
     }
 
     clicker = !clicker;

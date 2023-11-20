4e99ed9d59c40755a1d3dd1581095e286f7af091
Lucas Lima Rodrigues <138540444+lucaslimal04@users.noreply.github.com>
2023-11-20 17:55:26 -0300

    novo background

--- script/script_pag1.js
+++ script/script_pag1.js
@@ -33,17 +33,20 @@ var clickerSchemas = true;
 var localschemas = document.querySelector('#localschemas');
 var schemastxtpos = document.querySelector('#schemastxtpos');
 var solucaoschemas = document.querySelector('#solucaoschemas');
+var main = document.querySelector('main');
 
 schemas.addEventListener('click', abrirSchemas);
 
 function abrirSchemas() {
     if (clickerSchemas) {
+        main.style.background = '#ffffffd0';
         schemastxt.innerHTML += "<p>O SIAFW está mapeado incorretamente, clique com o botão direito do mouse no ícone do SIAFW e selecione abrir local do arquivo, o diretório dos arquivos que o SIAFW está, é direcionado pela rede conforme mostram as imagens.</p>";
         localschemas.style.display = 'block';
         schemastxtpos.innerHTML += "<p>O Sistema precisa ser direcionado pelo servidor, faça o mapeamento do SIAFW de acordo com a imagem e abra o SIAFW por esse mapeamento que conseguirá emitir notas normalmente.</p>";
         solucaoschemas.style.display = 'block';
     }
     else {
+        main.style.background = '#ffffff5d';
         schemastxt.innerHTML = "";
         localschemas.style.display = 'none';
         schemastxtpos.innerHTML = "";
@@ -62,10 +65,12 @@ fieldCelular.addEventListener('click', abrirFieldCelular);
 
 function abrirFieldCelular() {
     if (clickerField) {
+        main.style.background = '#ffffffd0';
         fieldCelulartxt.innerHTML += "<p>Está faltando essa coluna no banco de dados, rodar os comandos versaoxxx, versao180823190 (o número da versão) e o atualiza programas na manutenção de arquivos para criar novamente essa coluna.</p>";
         fieldCelularimg.style.display = 'block'
     }
     else {
+        main.style.background = '#ffffff5d';
         fieldCelulartxt.innerHTML = "";
         fieldCelularimg.style.display = 'none';
     }
@@ -82,12 +87,14 @@ erroDiButton.addEventListener('click', abrirErroDi);
 
 function abrirErroDi() {
     if(clickerDi){
+        main.style.background = '#ffffffd0';
         errodiimg.style.display = 'block';
         erroditxt.innerHTML += "<p>Esse erro ocorre devido à Substituição Tributária (ST) referente ao produto. A ST do produto vai estar configurada como 100, o que indica que o produto foi adquirido diretamente por importação. Nesse caso, o sistema exige que informações de importação sejam fornecidas.</p>";
         erroditxtpos.innerHTML += "<p>No entanto, se o cliente não adquiriu o produto diretamente por importação, é aconselhável utilizar a ST 200 ou 000. Recomenda-se confirmar essa classificação com a contabilidade para garantir a conformidade com a legislação tributária.</p>";
         
     }
     else {
+        main.style.background = '#ffffff5d';
         errodiimg.style.display = 'none';
         erroditxt.innerHTML = "";
         erroditxtpos.innerHTML = "";
@@ -109,6 +116,7 @@ cfopexbutton.addEventListener('click', abrircfopex);
 
 function abrircfopex() {
     if(clickercfopex) {
+        main.style.background = '#ffffffd0';
         cfopeximg.style.display = 'block';
         cfopex.innerHTML += "<p>Primeiramente, deve ser verificado o CFOP que está sendo utilizado.</p>";
         cfopexpos.innerHTML += "<p>Caso o cadastro do cliente seja de outro estado, o CFOP utilizado seria com número inicial 6, o cadastro do cliente deve estar cadastrado como Outros.</p>";
@@ -116,6 +124,7 @@ function abrircfopex() {
         
     }
     else {
+        main.style.background = '#ffffff5d';
         cfopeximg.style.display = 'none';
         cfopex.innerHTML = "";
         cfopexpos.innerHTML = "";
@@ -138,6 +147,7 @@ csosnButton.addEventListener('click', abrirCsosn);
 
 function abrirCsosn() {
     if (clickercsosn) {
+        main.style.background = '#ffffffd0';
         csosnImg.style.display = 'block';
         csosnIn.innerHTML += `<ul>
             <li>102 - Tributada pelo Simples Nacional sem permissão de crédito;</li>
@@ -180,6 +190,7 @@ function abrirCsosn() {
         </tr>
     </table>`
     } else {
+        main.style.background = '#ffffff5d';
         csosnImg.style.display = 'none';
         csosnIn.innerHTML = "";
         csosnNumerado.innerHTML = "";
